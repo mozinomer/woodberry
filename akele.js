75b053d069bfca6bddb84eef61cbd2591786c29f
mozinomer <mozinomer12@gmail.com>
2020-10-13 11:42:57 +0500

    ad

--- akele.js
+++ akele.js
@@ -1,35 +1,6 @@
-// Hide header on scroll down
-var didScroll;
-var lastScrollTop = 5;
-var delta = 5;
-var navbarHeight = jQuery('header.main_header').outerHeight();
-jQuery(window).scroll(function(event){
-    didScroll = true;
-
-});
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
-jQuery()
-function hasScrolled() {
-    var st = jQuery(this).scrollTop();
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    if (st > lastScrollTop && st > navbarHeight){
-        jQuery('header.main_header').removeClass('scroll-down').addClass('scroll-up');
-    } else {
-        if(st + jQuery(window).height() < jQuery(document).height()) {
-            jQuery('header.main_header').removeClass('scroll-up').addClass('scroll-down');
-            if(jQuery(window).scrollTop() <= 50){
-            	 jQuery('header.main_header').removeClass('scroll-down'); 	
-              }	
-        }
-    }
-    lastScrollTop = st;
-}
+$(window).scroll(function(event){
+    ($(window).scrollTop()) > 40 ? $('header#headermain').addClass('activatedScroll') : $('header#headermain').removeClass('activatedScroll')
+})
 $('div#closer').click( function(e) {
     $('.modalContainer').delay(4000).hide();
     $('body').css({'overflow' : 'auto'})
